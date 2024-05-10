a7420dcd057300f8508d44531c80034ea9fd4782
Rishabh Kumar <rsbkme@gmail.com>
2024-05-10 16:13:07 +0530

    config: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'react-native',
+            importNames: ['SafeAreaView'],
+            message:
+              'Import SafeAreaView from react-native-safe-area-context instead',
+          },
+        ],
+      },
+    ],
+  },
 };
